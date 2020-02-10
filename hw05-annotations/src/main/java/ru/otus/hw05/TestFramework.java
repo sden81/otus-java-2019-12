9539abf6ba2433d819832aab61820c89a956f59b
denis <sden81@gmail.com>
2020-02-10 22:36:56 +0300

    fix

--- hw05-annotations/src/main/java/ru/otus/hw05/TestFramework.java
+++ hw05-annotations/src/main/java/ru/otus/hw05/TestFramework.java
@@ -1,19 +1,19 @@
 package ru.otus.hw05;
 
 import ru.otus.hw05.annotations.Before;
-import ru.otus.hw05.annotations.End;
+import ru.otus.hw05.annotations.After;
 import ru.otus.hw05.annotations.Test;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TestFramework {
     protected Class testClass;
-    protected ArrayList<Method> beforeMethods = new ArrayList<>();
-    protected ArrayList<Method> testMethods = new ArrayList<>();
-    protected ArrayList<Method> endMethods = new ArrayList<>();
+    protected List<Method> beforeMethods = new ArrayList<>();
+    protected List<Method> testMethods = new ArrayList<>();
+    protected List<Method> endMethods = new ArrayList<>();
 
     public TestFramework(Class testClass) {
         this.testClass = testClass;
@@ -30,7 +30,7 @@ public class TestFramework {
                 testMethods.add(method);
                 continue;
             }
-            if (method.getAnnotation(End.class) instanceof End) {
+            if (method.getAnnotation(After.class) instanceof After) {
                 endMethods.add(method);
             }
         }
@@ -41,31 +41,32 @@ public class TestFramework {
             throw new RuntimeException("Empty tests");
         }
 
-        int successTests = 0;
         int faultTests = 0;
         int errorTests = 0;
 
-
-        Object testSuiteInstance = testClass.getDeclaredConstructor().newInstance();
         for (Method testMethod : testMethods) {
+            Object testSuiteInstance = testClass.getDeclaredConstructor().newInstance();
+            System.out.println("");
             System.out.println("-------------------------");
             System.out.printf("Run test %s", testMethod.getName());
             System.out.println("");
 
-            runBeforeEnd(beforeMethods, testSuiteInstance);
+            if (!runBeforeAfter(beforeMethods, testSuiteInstance)){
+                faultTests++;
+                continue;
+            }
 
             //run test
             try {
                 Object testResult = testMethod.invoke(testSuiteInstance);
                 if (testResult instanceof Boolean) {
-                    if ((boolean) testResult) {
-                        successTests++;
-                    } else {
+                    if (!(boolean) testResult) {
                         faultTests++;
                     }
                 }
             } catch (Exception e) {
                 errorTests++;
+                System.out.println("");
                 System.out.printf("Exception %s in method %s with description %s%s",
                         e.getClass().toString(),
                         testMethod.getName(),
@@ -74,31 +75,38 @@ public class TestFramework {
                 );
             }
 
-            runBeforeEnd(endMethods, testSuiteInstance);
+            if (!runBeforeAfter(endMethods, testSuiteInstance)){
+                faultTests++;
+            }
         }
+        System.out.println("");
+        System.out.println("-------------------------");
         System.out.printf("Success tests: %d Fault tests: %d Error tests: %d",
-                successTests,
+                testMethods.size() - faultTests - errorTests,
                 faultTests,
                 errorTests
         );
     }
 
-    protected void runBeforeEnd(ArrayList<Method> methodsList, Object instance) throws InvocationTargetException, IllegalAccessException {
+    protected boolean runBeforeAfter(List<Method> methodsList, Object instance) throws InvocationTargetException, IllegalAccessException {
         if (methodsList.size() < 1) {
-            return;
+            return true;
         }
 
         for (Method runMethod : methodsList) {
             try {
                 runMethod.invoke(instance);
-            } catch (RuntimeException e) {
+            } catch (Exception e) {
                 System.out.printf("Exception %s in method %s with description",
                         e.getClass().toString(),
                         runMethod.getName(),
                         e.getMessage()
                 );
+                return false;
             }
         }
+
+        return true;
     }
 
 }
--- hw05-annotations/src/main/java/ru/otus/hw05/TestSuite.java
+++ hw05-annotations/src/main/java/ru/otus/hw05/TestSuite.java
@@ -1,48 +1,48 @@
 package ru.otus.hw05;
 
 import ru.otus.hw05.annotations.Before;
-import ru.otus.hw05.annotations.End;
+import ru.otus.hw05.annotations.After;
 import ru.otus.hw05.annotations.Test;
 
 public class TestSuite {
 
     @Before
-    public void beforeTest1(){
+    public void beforeTest1() {
         System.out.println("Execute TestSuite.beforeTest1");
     }
 
     @Before
-    public void beforeTest2(){
+    public void beforeTest2() {
         System.out.println("Execute TestSuite.beforeTest2");
     }
 
     @Test
-    public boolean successTest(){
+    public boolean successTest() {
         System.out.println("Execute TestSuite.testSuccess");
 
         return true;
     }
 
     @Test
-    public boolean failedTest(){
+    public boolean failedTest() {
         System.out.println("Execute TestSuite.failedTest");
 
         return false;
     }
 
     @Test
-    public boolean errorTest(){
+    public boolean errorTest() {
         System.out.println("Execute TestSuite.errorTest");
-        throw new RuntimeException("Error in test");
+        throw new AssertionError("Error in test");
     }
 
-    @End
-    public void endTest1(){
+    @After
+    public void endTest1() {
         System.out.println("Execute TestSuite.endTest1");
     }
 
-    @End
-    public void endTest2(){
+    @After
+    public void endTest2() {
         System.out.println("Execute TestSuite.endTest2");
     }
 }
